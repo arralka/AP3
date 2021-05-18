235bd20ed6d8ac7cd2889cafd2fce3d665f0fefc
arralka <arralka@protonmail.com>
2021-05-18 16:53:55 +0930

    Ensure word is in lower case

--- syllables.py
+++ syllables.py
@@ -1,9 +1,13 @@
 import json
 import urllib.request as urllib2
+
+
 def syllableCount(word):
+	word = word.lower()
 	url='https://api.datamuse.com/words?md=s&max=1&sp=' + word
 	data = json.load(urllib2.urlopen(url))
 	if data[0]['word'] != word:
 		print("API error")
+		return -1
 	return data[0]['numSyllables']
 	
